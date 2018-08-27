9160b140754ab764b2173a62e7191ad41764a812
John Kreitlow <jkreitlow@jwplayer.com>
2018-08-27 11:22:20 -0400

    Use wp_safe_remote_get for VIP calls

--- jw-player/include/jwplayer-api.class.php
+++ jw-player/include/jwplayer-api.class.php
@@ -85,7 +85,7 @@ class JWPlayer_api {
 
 		$response = null;
 		if ( 'wpvip' == $this->_library ) {
-			$response = wp_remote_get( $url, array(
+			$response = wp_safe_remote_get( $url, array(
 				'timeout' => 5
 			) );
 		} else {
